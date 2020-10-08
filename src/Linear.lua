0f608cbeb0a11653be363d9cb821d1b0843537ae
Micah Hinckley <hinckleytmicah@gmail.com>
2020-10-07 19:16:55 -0700

    Add linear goal

---
+++ src/Linear.lua
@@ -0,0 +1,39 @@
+local Linear = {}
+Linear.__index = Linear
+
+function Linear.new(targetValue, options)
+    assert(targetValue, "Missing argument #1: targetValue")
+    
+	options = options or {
+        velocity = 1
+    }
+
+	return setmetatable({
+        _targetValue = targetValue,
+        _targetVelocity = options.velocity
+	}, Linear)
+end
+
+function Linear:step(state, dt)
+    local position = state.value
+	local velocity = self._targetVelocity
+	local goal = self._targetValue
+
+	local dPos = dt * velocity
+
+	local complete = math.abs(goal - position) <= dPos
+	position = position + dPos * (goal > position and 1 or -1)
+
+	if complete then
+		position = self._targetValue
+		velocity = 0
+	end
+
+	return {
+		complete = complete,
+		value = position,
+		velocity = velocity
+	}
+end
+
+return Linear
