d1da8acb9ebf53cd0fecc459a773177961f0e6b6
Reselim <notreselim@gmail.com>
2021-10-06 18:16:50 -0400

    Built and published by Moonwave

---
+++ assets/js/16d1736c.d9201e28.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[932],{97868:function(s){s.exports=JSON.parse('{"sourceUrl":"https://github.com/Reselim/Flipper/blob/master","baseUrl":"/Flipper/","classOrder":[]}')}}]);
--- assets/js/16d1736c.ebaf67f8.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[932],{97868:function(s){s.exports=JSON.parse('{"sourceUrl":"https://github.com/Reselim/Flipper/blob/master","baseUrl":"/flipper/","classOrder":[]}')}}]);
